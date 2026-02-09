f1b03263630167867038b0ce571bd4c4cc435974
nhtrung.fn@incotrade.vn <nhtrung.fn@incotrade.vn>
2026-02-09 11:37:55 +0700

    [Config] intit 2, update config tailwindcss and structure folder

---
+++ components/sections/Hero.vue
@@ -0,0 +1,7 @@
+<template>
+	<section class="relative h-screen w-full overflow-hidden">
+		<h1 class="text-4xl font-bold text-red-500 uppercase">
+			nguyen huu trung
+		</h1>
+	</section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Hero from "@/components/sections/Hero.vue";
+</script>
+
+<template>
+	<Hero />
+</template>
