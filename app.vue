9d61f9a29f3191886c09d651b0f142f7475cb83c
nhtrung.fn@incotrade.vn <nhtrung.fn@incotrade.vn>
2026-02-10 12:54:29 +0700

    [useHead] update domain origin and title

--- app.vue
+++ app.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
+const url = useRequestURL();
+const domain = url.origin;
+
 useHead({
-	title: "Nguyễn Hữu Trung | Senior Frontend Developer & UI/UX Specialist",
+	title: "Nguyễn Hữu Trung | Frontend Developer & UI/UX Specialist",
 	meta: [
 		{
 			name: "description",
 			content:
-				"Hơn 3 năm kinh nghiệm thực chiến tại Canh Cam & Incotrade. Chuyên gia Nuxt 3, Vue 3 và tối ưu hóa UI/UX Pixel-perfect.",
+				"Hơn 3 năm kinh nghiệm thực chiến tại Canh Cam & Incotrade. Tối ưu hóa UI/UX Pixel-perfect.",
 		},
 		// Open Graph (Dành cho Facebook, Zalo, LinkedIn)
 		{
 			property: "og:title",
-			content: "Nguyễn Hữu Trung | Portfolio Architect",
+			content: "Nguyễn Hữu Trung | Portfolio Frontend",
 		},
 		{
 			property: "og:description",
@@ -18,7 +21,7 @@ useHead({
 		},
 		{
 			property: "og:image",
-			content: "https://your-domain.com/images/og-image.jpg",
+			content: `${domain}/images/og-card.png`,
 		},
 		{ property: "og:type", content: "website" },
 		// Twitter Card
